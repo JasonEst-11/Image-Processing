48f071b2a77226a842b088ede23be9b5fdfc7208
Esteban <jesteban@totalware.gr>
2025-01-28 16:39:19 +0200

    pivot program

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
 ﻿using Test;
-using Test.Classes;
-using Test.Enums;
 ImageList.ClearOuptut();
 var images = ImageList.GetImages();
 
 //Set processing
-// ImageProcessingContext process = new(new Test.Classes.Test(225, 200));
+ImageProcessingContext process = new(new Test.Classes.Test(225, 200));
 // ImageProcessingContext process = new(new High(true,false,true));
-ImageProcessingContext process = new(new PlusMinus(100, Operations.Division));
+// ImageProcessingContext process = new(new Arithmetics(100, Operations.Division));
 int i = 0;
 foreach (string image in images)
 {
