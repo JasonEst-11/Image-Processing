dd92c616258145795080820c95f78ae962c3d01a
Jason Esteban <francisjaeson@gmail.com>
2025-01-28 21:06:25 +0200

    added more images for testing

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using Test;
+using Test.Classes;
+using Test.Enums;
 ImageList.ClearOuptut();
 var images = ImageList.GetImages();
 
 //Set processing
-ImageProcessingContext process = new(new Test.Classes.Test(225, 200));
+// ImageProcessingContext process = new(new Test.Classes.Test(100, 100));
 // ImageProcessingContext process = new(new High(true,false,true));
-// ImageProcessingContext process = new(new Arithmetics(100, Operations.Division));
+ImageProcessingContext process = new(new Arithmetics(100, Operations.Addition));
 int i = 0;
 foreach (string image in images)
 {
