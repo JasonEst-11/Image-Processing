98e703e17f19d105b3a2b991347c22bfc2b2c2a2
Jason Esteban <francisjaeson@gmail.com>
2025-03-08 00:42:03 +0200

    merge fix

--- Program.cs
+++ Program.cs
@@ -7,20 +7,8 @@ var images = ImageList.GetImages();
 //Set processing
 // ImageProcessingContext process = new(new Test.Classes.Test(null, 100));
 // ImageProcessingContext process = new(new High(true,false,true));
-<<<<<<< HEAD
 // ImageProcessingContext process = new(new Arithmetics(2, Operations.Multiplication));
 ImageProcessingContext process = new(new Invert());
-=======
-var imageName = "image1.jpg";
-ImageProcessingContext process = new(new Arithmetics(4, Operations.Division));
-ImageProcessing processing = new($"image_in/{imageName}", process);
-
-// First process will apply normally
-processing.ExecuteBottomHalf("BH.jpg");
-// Second process will apply on top of the first process 
-processing.ExecuteLeftHalf("LH.jpg");
-
->>>>>>> 08461ad7f9cc16d83dcc5ef4f60fb12189febfe4
 int i = 0;
 foreach (string image in images)
 {
